f9e9020cb5df6f304972f594b7537776cb814f99
John Doe <prof.narendra747@gmail.com>
2024-10-02 16:02:42 +0530

    created from string helper function

--- env/type.go
+++ env/type.go
@@ -1,8 +1,23 @@
 package env
 
+import (
+	"errors"
+	"strings"
+)
+
 type Type string
 
 const (
-	TypeProd  = Type("Production")
-	TypeLocal = Type("Local")
+	TypeProd  = Type("production")
+	TypeLocal = Type("local")
 )
+
+func FromString(str string) (Type, error) {
+	switch strings.ToLower(str) {
+	case string(TypeProd):
+		return TypeProd, nil
+	case string(TypeLocal):
+		return TypeLocal, nil
+	}
+	return "", errors.New("invalid type")
+}
